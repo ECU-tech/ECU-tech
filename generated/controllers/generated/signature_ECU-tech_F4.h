a14d2c44e179012850d6e32c9ac64b379d79effd
GitHub git update Action <action@github.com>
2024-07-17 16:49:29 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_ECU-tech_F4.h
+++ generated/controllers/generated/signature_ECU-tech_F4.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3379610609
-#define TS_SIGNATURE "rusEFI main.2024.07.16.ECU-tech_F4.3379610609"
+#define SIGNATURE_HASH 628814068
+#define TS_SIGNATURE "rusEFI main.2024.07.17.ECU-tech_F4.628814068"
